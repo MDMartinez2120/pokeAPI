2269563f5e25b3b3827c61f011695ca7a859ab2f
Matthew Martinez <matt.martinez2120@gmail.com>
2022-07-01 13:58:07 -0500

    refactored js

--- js/main.js
+++ js/main.js
@@ -2,27 +2,63 @@ $("button").click(function (){
     searchPokemon();
 })
 
-const mapResults = (data) => ` 
-    <div>
-        <img src="${data.name}">
-    </div>
-
-    <div class = "results">
-        <h2 class="text-white">${data.name}</h2>
-        <p class="text-white">${data.weight}</p>
-    </div>`
+const pokedex = document.getElementById('pokedex')
 
 const userSearch = $("#input").val();
 
-const pokeSearchUrl = `https://pokeapi.co/api/v2/pokemon/${userSearch}`
+const pokeSearchUrl = `https://pokeapi.co/api/v2/pokemon/1`
 
-function searchPokemon(e){
+function fetchPokemon(e){
     fetch(pokeSearchUrl).then((response) => response.json()).then((data) => {
-        const pokeResults = data.map(mapResults);
-        return $(".results").html(pokeResults)
+        console.log(data);
+        const pokemon = {
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types.map((type) => type.type.name).join(', ')
+        };
+        console.log(pokemon);
+    })
 
-        })
-    }
+    const displayPokemon = (pokemon) => {
+        const pokemonHTMLString =  pokemon.map(pokeman => `
+        <li>
+            <img src = "${pokeman.image}"/>
+            <h2>${pokeman.id}. ${pokeman.name}</h2>
+            <p>Type: ${pokeman.type}</p>
+        </li>
+    `).join("")
+        pokedex.innerHTML = pokemonHTMLString;
+    };
+}
+
+fetchPokemon();
+
+
+
+
+
+// const mapResults = (data) => `
+//     <div>
+//         <img src="${data.name}">
+//     </div>
+//
+//     <div class = "results">
+//         <h2 class="text-white">${data.name}</h2>
+//         <p class="text-white">${data.weight}</p>
+//     </div>`
+//
+// const userSearch = $("#input").val();
+//
+// const pokeSearchUrl = `https://pokeapi.co/api/v2/pokemon/${userSearch}`
+//
+// function searchPokemon(e){
+//     fetch(pokeSearchUrl).then((response) => response.json()).then((data) => {
+//         const pokeResults = data.map(mapResults);
+//         return $(".results").html(pokeResults)
+//
+//         })
+//     }
 
 
 
